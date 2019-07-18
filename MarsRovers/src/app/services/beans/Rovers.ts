fa9b0fc4b7cd3b775217536de9657cfd622b3fd5
MichaelGoodwin <7521194+MichaelGoodwin@users.noreply.github.com>
2019-07-17 19:17:30 -0700

    More beans

---
+++ MarsRovers/src/app/services/beans/Rovers.ts
@@ -0,0 +1,7 @@
+import { Cameras } from './Cameras';
+
+export const Rovers = {
+  CURIOSITY: [Cameras.FHAZ, Cameras.RHAZ, Cameras.MAST, Cameras.CHEMCAM, Cameras.MAHLI, Cameras.MARDI, Cameras.NAVCAM],
+  OPPORTUNITY: [Cameras.FHAZ, Cameras.RHAZ, Cameras.NAVCAM, Cameras.PANCAM, Cameras.MINITES],
+  SPIRIT: [Cameras.FHAZ, Cameras.RHAZ, Cameras.NAVCAM, Cameras.PANCAM, Cameras.MINITES]
+};
