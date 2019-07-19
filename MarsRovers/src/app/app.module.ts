8098a52e9247ae0059271033a9932ae0f106c5af
MichaelGoodwin <7521194+MichaelGoodwin@users.noreply.github.com>
2019-07-18 19:26:18 -0700

    Add ngx-lightbox dependency

--- MarsRovers/src/app/app.module.ts
+++ MarsRovers/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { LoadingComponent } from './loading/loading.component';
+import { LightboxModule } from 'ngx-lightbox';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { LoadingComponent } from './loading/loading.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    LightboxModule,
     AppRoutingModule
   ],
   providers: [],
