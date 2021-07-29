0b276f8277552d22150f51c44b9f4951ed8708f7
MichaelGoodwin <7521194+MichaelGoodwin@users.noreply.github.com>
2021-07-28 19:41:44 -0700

    Add Missing Perseverance camera

--- MarsRovers/src/app/services/beans/RoverCameras.ts
+++ MarsRovers/src/app/services/beans/RoverCameras.ts
@@ -7,5 +7,5 @@ export const RoverCameras = {
   PERSEVERANCE: [Cameras.EDL_RUCAM, Cameras.EDL_RDCAM, Cameras.EDL_DDCAM, Cameras.EDL_PUCAM1, Cameras.EDL_PUCAM2,
     Cameras.NAVCAM_LEFT, Cameras.NAVCAM_RIGHT, Cameras.MCZ_RIGHT, Cameras.MCZ_LEFT, Cameras.FRONT_HAZCAM_LEFT_A,
     Cameras.FRONT_HAZCAM_RIGHT_A, Cameras.REAR_HAZCAM_LEFT, Cameras.REAR_HAZCAM_RIGHT, Cameras.SKYCAM,
-    Cameras.SHERLOC_WATSON]
+    Cameras.SHERLOC_WATSON, Cameras.SUPERCAM_RMI]
 };
